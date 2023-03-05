37cc0aed03d213570da67d36b1ac9ba32a8b82ec
sachin <sachin@gmail.com>
2023-03-05 10:53:37 +0530

    ADded header into backend

--- Routes/url.route.js
+++ Routes/url.route.js
@@ -2,6 +2,14 @@ const express = require("express");
 const urlRoute = express.Router();
 const urlController = require("../Controller/url.controller");
 
+urlRoute.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 urlRoute.post("/createurl", urlController.createUrl);
 
 module.exports = urlRoute;
